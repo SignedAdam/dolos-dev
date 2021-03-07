d4a1ecbe6d5f7396ee622805317c229a4df59c3c
Adam Albastov <arise@live.com>
2021-03-07 14:54:09 +0100

    aycd v1 (imcomplete)

--- pkg/driver/captcha/aycd/aycd.go
+++ pkg/driver/captcha/aycd/aycd.go
@@ -1,38 +1,30 @@
 package aycd
 
 import (
+	"adam/learn-gitlab/pkg/helperfuncs"
 	"fmt"
 
 	"gitlab.com/aycd-inc/autosolve-clients/autosolve-client-go"
 )
 
+//CaptchaSolver represents an instance of this driver
 type CaptchaSolver struct {
+	solvedTokens map[string]chan string
 }
 
+//New creates a new instance of the AYCD captcha solver driver
 func New() (*CaptchaSolver, error) {
+	solver := &CaptchaSolver{
+		solvedTokens: make(map[string]chan string),
+	}
 	accessToken := ""
 	apiKey := ""
-	//First, create listener functions, which will be passed to the module and called when tokens are received/cancelled
 
-	//Establishes listener function to receive token responses
-	var tokenListener autosolve.CaptchaTokenResponseListener = func(tokenResponse autosolve.CaptchaTokenResponse) {
-		//handle token response here
-	}
-
-	//Establishes listener function to receive cancel token responses
-	var tokenCancelListener autosolve.CaptchaTokenCancelResponseListener = func(cancelTokenResponse autosolve.CaptchaTokenCancelResponse) {
-		//handle cancel response here
-	}
-
-	//Establishes listener function to receive status updates from AutoSolve
-	var statusListener autosolve.StatusListener = func(status autosolve.Status) {
-		//handle status message here
-	}
-
-	//Establishes listener function to receive errors
-	var errorListener autosolve.ErrorListener = func(err error) {
-		//handle error message here
-	}
+	//init listener callbacks
+	var tokenListener autosolve.CaptchaTokenResponseListener = solver.handleResponse
+	var tokenCancelListener autosolve.CaptchaTokenCancelResponseListener = solver.handleCancel
+	var statusListener autosolve.StatusListener = solver.handleStatusUpdate
+	var errorListener autosolve.ErrorListener = solver.handleError
 
 	//To initiate the Go module, call the following function, with the functions we defined above:
 	err := autosolve.Load("your-client-id", tokenListener, tokenCancelListener, statusListener, errorListener)
@@ -46,11 +38,9 @@ func New() (*CaptchaSolver, error) {
 		return nil, fmt.Errorf("Failed to connect to captcha solver (%v)", err)
 	}
 
-	//Result is a string "enum", outlined as a type ConnectResult with multiple types. You can process the response below
-
 	switch result {
 	case autosolve.Success:
-		print("Successful Connection")
+		fmt.Println("Successful Connection")
 	case autosolve.InvalidClientId:
 		return nil, fmt.Errorf("Invalid Client Key")
 	case autosolve.InvalidAccessToken:
@@ -61,10 +51,55 @@ func New() (*CaptchaSolver, error) {
 		return nil, fmt.Errorf("Invalid Credentials")
 	}
 
-	solver := &CaptchaSolver{}
-
 	return solver, nil
 }
-func solveCaptcha(url string) {
-	autosolve.SendTokenRequest(autosolve.CaptchaTokenRequest{})
+func (solver *CaptchaSolver) solveCaptcha(url string, responseToken chan string) error {
+
+	request := autosolve.CaptchaTokenRequest{
+		TaskId: helperfuncs.GenerateRandomString(10),
+
+		Url: url,
+
+		//Public ReCaptcha key for a given site
+		SiteKey: "TODO TODO TODO",
+
+		//Version of Captcha
+		//Options:
+		/*
+		  reCaptcha V2 Checkbox is 0
+		  reCaptcha V2 Invisible is 1
+		  reCaptcha V3 Score is 2
+		  hCaptcha Checkbox is 3
+		  hCaptcha Invisible is 4
+		  GeeTest is 5
+		*/
+		Version: 0,
+	}
+	err := autosolve.SendTokenRequest(request)
+	if err != nil {
+		return fmt.Errorf("Failed to send request to solve captcha (%v)", err)
+	}
+
+	solver.solvedTokens[request.TaskId] = responseToken
+
+	return nil
+
+}
+
+func (solver *CaptchaSolver) handleResponse(tokenResponse autosolve.CaptchaTokenResponse) {
+	solver.solvedTokens[tokenResponse.TaskId] <- tokenResponse.Token
+}
+
+func (solver *CaptchaSolver) handleCancel(cancelTokenResponse autosolve.CaptchaTokenCancelResponse) {
+	for _, request := range cancelTokenResponse.Requests {
+		solver.solvedTokens[request.TaskId] <- "1"
+	}
+}
+
+func (solver *CaptchaSolver) handleStatusUpdate(status autosolve.Status) {
+	fmt.Println(fmt.Errorf("AYCD: connection status updated: %v", status))
+}
+
+func (solver *CaptchaSolver) handleError(err error) {
+	fmt.Println(fmt.Errorf("AYCD: error (%v)", err))
 }
